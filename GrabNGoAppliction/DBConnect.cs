850384f784b9e5a3d2852fad049e6e50e57193c0
HimalayLevharkar <142683903+HimalayLevharkar@users.noreply.github.com>
2025-02-02 00:18:10 +0530

    Add files via upload

---
+++ GrabNGoAppliction/DBConnect.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace GrabNGoAppliction
+{
+    class DBConnect
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-8TM23T0M\SQLEXPRESS;Initial Catalog=GrabNGo_DB;Integrated Security=True;TrustServerCertificate=True");
+        public SqlConnection GetCon()
+        {
+            return con;
+        }
+        public void OpenCon()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void CloseCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
